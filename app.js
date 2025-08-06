2f46df22899e8a4788a336a06ff92bbf57631072
Rizky Al Farid Hafizh <rizkyalfaridhafizh@Rizkys-MacBook-Air.local>
2025-08-06 10:37:08 +0700

    styling

--- app.js
+++ app.js
@@ -50,8 +50,10 @@ app.use('/pemesanan', pemesananRouter);
 app.use((_req, _res, next) => next(createError(404)));
 
 // error handler
-app.use((err, res, _next) => {
+app.use((err, req, res, _next) => {
     res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
     res.status(err.status || 500);
     res.render('error');
 });
