c501dc1242c2e7c1414f5b74fe38e03c18b561df
Ims4d <irsadkun5@gmail.com>
2025-08-09 13:44:25 +0700

    Remake index.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (_req, res, _next) => {
-    res.render('index', { title: 'Teras Rasa' });
+    res.render('index', { title: 'RMS Teras Rasa' });
 });
 
 export default router;
