3e483310cc94d0f4a0490d1b8c3b213d55d11891
Ims4d <irsadkun5@gmail.com>
2025-08-09 11:14:16 +0700

    Brrr

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (_req, res, _next) => {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Teras Rasa' });
 });
 
 export default router;
